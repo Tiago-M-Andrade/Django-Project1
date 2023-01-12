d5ea5ef70a577040ec3468786a4f1ac11bc3aa4a
Tiago Martins Andrade <x15_tiago@hotmail.com>
2023-01-12 09:32:31 +0000

    Add Static Files and Config.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from recipes.views import about_view, contact_view, home_view
+from recipes.views import home_view
 
 urlpatterns = [
     path('', home_view),
-    path('about/', about_view),
-    path('contact/', contact_view),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, 'recipes/home.html', context={'name': 'Tiago Martins'})
-
-
-def about_view(request):
-    return HttpResponse('About')
-
-
-def contact_view(request):
-    return HttpResponse('Contact')
+    return render(request, 'recipes/pages/home.html',
+                  context={'name': 'Tiago Martins'})
